80c852cdd2f351241ad2b2c177737a28cde4f532
Adithya  Kasali <adithyakasali@adithyas-mbp-2.home>
2020-04-07 17:50:16 -0400

    added annuity payment portion.

--- js/main.js
+++ js/main.js
@@ -8,29 +8,36 @@ function range (start, end, step) {
     return arrayNum;
 };
 
-function evaluate(xAxis, p, r) {
+function evaluate(xAxis, p, gr, pr) {
   let arr = [];
   for(let i = 0; i < xAxis.length; i++) {
-    arr.push(p* Math.pow(1 + r, xAxis[i]));
+    arr.push(p * Math.pow( (((1-(pr/2))*(1 + gr)) - (pr/2)), xAxis[i]));
+    console.log("" + ((1-(pr/2))*(1 + gr)) - (pr/2));
   }
   return arr;
 };
 let myChart;
 let principal = 10000;
 let startTime = 5;
-let endTime = 50;
+let endTime = 25;
 let increment = 5;
-let rate = .05;
+let growthRate = .075;
+let paymentRate = .05;
 let xAxis = range(startTime, endTime, increment);
-let yAxis = evaluate(xAxis, principal, rate);
+let yAxis = evaluate(xAxis, principal, growthRate, paymentRate);
 let clicked = false;
 myChart = runGraph(yAxis);
 
 function myFunction() {
 
     let newP = document.getElementById('principal').value;
-    yAxis = evaluate(xAxis, newP, rate);
-
+    let newGr = document.getElementById('rate').value;
+    if(newGr > 10 || newGr < 0) {
+      alert("Pick interest rates between 0 and 10");
+      newGr = 7.5;
+    }
+    newGr = newGr/100 ;
+    yAxis = evaluate(xAxis, newP, newGr, paymentRate);
 
     myChart.destroy();
     myChart = runGraph(yAxis);
@@ -42,8 +49,9 @@ function myFunction() {
     clicked = true;
 
 }
+
 function clear(table) {
-  for(let i = 0; i < xAxis.length + 1; i++){
+  for(let i = 0; i <= xAxis.length; i++){
     table.deleteRow(0);
   }
 };
@@ -52,15 +60,19 @@ function generateTable(table, xAxis, yAxis) {
       let row = table.insertRow(0);
       let cell1 = row.insertCell(0);
       let cell2 = row.insertCell(1);
+      let cell3 = row.insertCell(2);
       cell1.innerHTML = "<td>" + xAxis[i] + "</td>";
-      cell2.innerHTML = "<td>" + "$" +Math.trunc(yAxis[i])+ "</td>";
+      cell2.innerHTML = "<td>" + "$" + Math.trunc(yAxis[i])+ "</td>";
+      cell3.innerHTML = "<td>" + "$" + Math.trunc(yAxis[i]*paymentRate)+ "</td>";
     }
     let header = table.createTHead();
     let row = header.insertRow(0);
     let cell1 = row.insertCell(0);
     let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
     cell1.innerHTML = "<td>Years</td>";
     cell2.innerHTML = "<td>Donation</td>";
+    cell3.innerHTML = "<td>Payment</td>";
 };
 
 function runGraph(ydata) {
