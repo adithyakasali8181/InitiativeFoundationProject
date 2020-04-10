52d87f5d4223ee4de16eede6329cfcae09aac33e
Adithya  Kasali <adithyakasali@adithyas-mbp-2.home>
2020-04-10 11:42:05 -0400

    added total payment column and added admin fee calculation

--- js/main.js
+++ js/main.js
@@ -16,15 +16,34 @@ function evaluate(xAxis, p, gr, pr) {
   }
   return arr;
 };
+function calcNetGrant(xAxis, yAxis) {
+  let arr = [];
+  for(let i = 0; i < xAxis.length; i++) {
+    arr.push(yAxis[i]*(paymentRate - feeRate));
+  }
+  return arr;
+}
+function calcCumulGrant(netGrant) {
+  let arr = [];
+  let sum = 0;
+  for(let i = 0; i < netGrant.length; i++) {
+    sum += netGrant[i];
+    arr.push(sum);
+  }
+  return arr;
+}
 let myChart;
 let principal = 10000;
-let startTime = 5;
+let startTime = 1;
 let endTime = 25;
-let increment = 5;
+let increment = 1;
 let growthRate = .075;
 let paymentRate = .05;
+let feeRate = 0.01;
 let xAxis = range(startTime, endTime, increment);
 let yAxis = evaluate(xAxis, principal, growthRate, paymentRate);
+let netGrant = calcNetGrant(xAxis, yAxis); // calculates (spending - fee) per year
+let cumulGrant = calcCumulGrant(netGrant); // calculate cumulative grant amount per year
 let clicked = false;
 myChart = runGraph(yAxis);
 
@@ -38,6 +57,8 @@ function myFunction() {
     }
     newGr = newGr/100 ;
     yAxis = evaluate(xAxis, newP, newGr, paymentRate);
+    netGrant = calcNetGrant(xAxis, yAxis);
+    cumulGrant = calcCumulGrant(netGrant);
 
     myChart.destroy();
     myChart = runGraph(yAxis);
@@ -45,34 +66,38 @@ function myFunction() {
     if(clicked) {
       clear(table);
     }
-    generateTable(table, xAxis, yAxis);
+    generateTable(table, xAxis, yAxis, netGrant, cumulGrant);
     clicked = true;
 
 }
 
 function clear(table) {
-  for(let i = 0; i <= xAxis.length; i++){
+  for(let i = 0; i <= 5; i++){
     table.deleteRow(0);
   }
 };
-function generateTable(table, xAxis, yAxis) {
-    for(let i = xAxis.length - 1; i >= 0; i--) {
+function generateTable(table, xAxis, yAxis, netGrant, cumulGrant) {
+    for(let i = 24; i >= 4; i = i - 5) {
       let row = table.insertRow(0);
       let cell1 = row.insertCell(0);
       let cell2 = row.insertCell(1);
       let cell3 = row.insertCell(2);
+      let cell4 = row.insertCell(3);
       cell1.innerHTML = "<td>" + xAxis[i] + "</td>";
       cell2.innerHTML = "<td>" + "$" + Math.trunc(yAxis[i])+ "</td>";
-      cell3.innerHTML = "<td>" + "$" + Math.trunc(yAxis[i]*paymentRate)+ "</td>";
+      cell3.innerHTML = "<td>" + "$" + Math.trunc(netGrant[i])+ "</td>";
+      cell4.innerHTML = "<td>" + "$" + Math.trunc(cumulGrant[i])+ "</td>";
     }
     let header = table.createTHead();
     let row = header.insertRow(0);
     let cell1 = row.insertCell(0);
     let cell2 = row.insertCell(1);
     let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
     cell1.innerHTML = "<td>Years</td>";
     cell2.innerHTML = "<td>Donation</td>";
     cell3.innerHTML = "<td>Payment</td>";
+    cell4.innerHTML = "<td>Total Payment</td>";
 };
 
 function runGraph(ydata) {
